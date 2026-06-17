1811309e92d95db8851cb0cf0be61d1f26852448
ll_git <minareg37@gmail.com>
2026-06-17 16:26:59 +0200

    fixed build error

--- src/pages/TicketSaleHistory.tsx
+++ src/pages/TicketSaleHistory.tsx
@@ -1,7 +1,7 @@
 import { AiOutlineSearch } from "react-icons/ai"
 import { BiCalendarAlt } from "react-icons/bi"
-import TableTwo from "../components/TableTwo"
-import useTickets, { TicketQuery } from "../hooks/useTickets";
+/* import TableTwo from "../components/TableTwo"
+import useTickets, { TicketQuery } from "../hooks/useTickets"; */
 import CustomDatePicker from "../components/CustomDatePicker";
 import { useState } from "react";
 import { TripQuery } from "../hooks/useTrips";
@@ -10,59 +10,59 @@ import { format } from "date-fns";
 
 function TicketSaleHistory() {
   const [tripQuery, setTripQuery] = useState<TripQuery>({} as TripQuery);
-  const [ticketQuery,] = useState<TicketQuery>({} as TicketQuery);
-  const { data: tickets } = useTickets(ticketQuery);
-   const [open, setOpen] = useState(false);
-    const [val, setVal] = useState<Date | [Date, Date] | null>(null);
-    const handleSelectDate = (val: Date | [Date, Date] | null) => {
-        if (!val) return;
-        if (!Array.isArray(val))
-          setTripQuery({
-            ...tripQuery,
-            departureTime: `${format(val, "d/M/yyyy HH'H'00")}`,
-          });
-      };
-    return (
-        <div className="mt-5 m-5 ml-3 dark:text-white">
-            <div className="flex items-center space-x-3 p-3 border-1 border-neutral-200 rounded-xl text-sm max-w-2xl">
-                    <AiOutlineSearch size={20} />
-                    <form action="">
-                      <input
-                        type="text"
-                        placeholder="Enter  destination..."
-                        className="placeholder:text-brand2 outline-none"
-                      />
-                    </form>
-                  </div>
-                  <div className="mt-3 mb-10 justify-self-end">
-                     <div className="relative ">
-                               <div
-                                 onClick={() => setOpen(!open)}
-                                 className="border-1 border-neutral-200 rounded-sm w-fit p-1 text-sm flex items-center space-x-2 cursor-pointer"
-                               >
-                                 <p>
-                                   {val ? format(val as Date, "PPpp") : "Choose date and time"}
-                                 </p>
-                                 <BiCalendarAlt size={14} />
-                               </div>
-                               <div className="absolute right-0 z-20 top-5">
-                                 <CustomDatePicker
-                                   mode="single"
-                                   withTime
-                                   isOpen={open}
-                                   onClose={() => setOpen(false)}
-                                   onChange={(selectedVal) => {
-                                     handleSelectDate(selectedVal);
-                   
-                                     setVal(selectedVal);
-                                   }}
-                                 />
-                               </div>
-                             </div>
-                  </div>
-                  <TableTwo tableData={tickets?.tickets ?? []} click/>
+  // const [ticketQuery,] = useState<TicketQuery>({} as TicketQuery);
+  // const { data: tickets } = useTickets(ticketQuery);
+  const [open, setOpen] = useState(false);
+  const [val, setVal] = useState<Date | [Date, Date] | null>(null);
+  const handleSelectDate = (val: Date | [Date, Date] | null) => {
+    if (!val) return;
+    if (!Array.isArray(val))
+      setTripQuery({
+        ...tripQuery,
+        departureTime: `${format(val, "d/M/yyyy HH'H'00")}`,
+      });
+  };
+  return (
+    <div className="mt-5 m-5 ml-3 dark:text-white">
+      <div className="flex items-center space-x-3 p-3 border-1 border-neutral-200 rounded-xl text-sm max-w-2xl">
+        <AiOutlineSearch size={20} />
+        <form action="">
+          <input
+            type="text"
+            placeholder="Enter  destination..."
+            className="placeholder:text-brand2 outline-none"
+          />
+        </form>
+      </div>
+      <div className="mt-3 mb-10 justify-self-end">
+        <div className="relative ">
+          <div
+            onClick={() => setOpen(!open)}
+            className="border-1 border-neutral-200 rounded-sm w-fit p-1 text-sm flex items-center space-x-2 cursor-pointer"
+          >
+            <p>
+              {val ? format(val as Date, "PPpp") : "Choose date and time"}
+            </p>
+            <BiCalendarAlt size={14} />
+          </div>
+          <div className="absolute right-0 z-20 top-5">
+            <CustomDatePicker
+              mode="single"
+              withTime
+              isOpen={open}
+              onClose={() => setOpen(false)}
+              onChange={(selectedVal) => {
+                handleSelectDate(selectedVal);
+
+                setVal(selectedVal);
+              }}
+            />
+          </div>
         </div>
-    )
+      </div>
+      {/* <TableTwo tableData={tickets?.tickets ?? []} click/> */}
+    </div>
+  )
 }
 
 export default TicketSaleHistory
